1908743b7512971bdf4535c453631476cb8d1d2c
Rani Saad <rsaad@live.ca>
2021-05-05 00:56:08 -0400

    Final html and css changes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,26 +1,24 @@
-const path = require("path");
-const router = require("express").Router();
-const animalRoutes = require("../apiRoutes/animalRoutes.js");
-const zookeeperRoutes = require("../zookeeperRoutes/zookeeperRoutes.js");
-router.use(require("./zookeeperRoutes"));
+const path = require('path');
+const router = require('express').Router();
 
-router.use(animalRoutes);
-router.use(zookeeperRoutes);
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/animals.html"));
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
 
-router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+router.get('/aquarium', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
